d69798ae9acaec0dd73393fd75bf20aed31452de
Eric Scott Freeman <ericnorway@users.noreply.github.com>
2016-10-10 12:20:56 -0500

    Needed to handle null duration values.
    Needed to handle null duration values in the SQL INSERT.

--- QueryBuilder.cs
+++ QueryBuilder.cs
@@ -15,20 +15,24 @@ namespace MyRunningApplication
 
 		// Builds the SQL command to insert a distance for a date.
 		public SqlCommand BuildAddDistanceCommand(SqlConnection conn, DateTime date, double distance, int? duration, string notes) {
-			SqlCommand cmd = new SqlCommand("INSERT INTO Distance (Date, Distance, Duration, Notes) VALUES (@0, @1, @2, @3);", conn);
-			cmd.Parameters.Add(new SqlParameter("0", date));
-			cmd.Parameters.Add(new SqlParameter("1", distance));
-			cmd.Parameters.Add(new SqlParameter("2", duration));
-			cmd.Parameters.Add(new SqlParameter("3", notes));
+			SqlCommand cmd = new SqlCommand("INSERT INTO Distance (Date, Distance, Duration, Notes) VALUES (@date, @distance, @duration, @notes);", conn);
+			cmd.Parameters.Add(new SqlParameter("date", date));
+			cmd.Parameters.Add(new SqlParameter("distance", distance));
+
+			var durPararmeter = new SqlParameter("duration", System.Data.SqlDbType.Int);
+			durPararmeter.Value = duration ?? (object)DBNull.Value; // ?? is a null-coalescing operator.
+			cmd.Parameters.Add(durPararmeter);
+
+			cmd.Parameters.Add(new SqlParameter("notes", notes));
 
 			return cmd;
 		}
 
 		// Builds the query for getting the total distance between two dates.
 		public SqlCommand BuildDistanceQuery(SqlConnection conn, DateTime start, DateTime end) {
-			SqlCommand cmd = new SqlCommand("SELECT SUM(Distance) FROM Distance WHERE Date >= @0 AND Date <= @1;", conn);
-			cmd.Parameters.Add(new SqlParameter("0", start));
-			cmd.Parameters.Add(new SqlParameter("1", end));
+			SqlCommand cmd = new SqlCommand("SELECT SUM(Distance) FROM Distance WHERE Date >= @begin AND Date <= @end;", conn);
+			cmd.Parameters.Add(new SqlParameter("begin", start));
+			cmd.Parameters.Add(new SqlParameter("end", end));
 
 			return cmd;
 		}
